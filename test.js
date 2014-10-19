94b27d6d1579af7455ae25fcc2c50d3b67651beb
wooorm <tituswormer@gmail.com>
2014-10-19 12:11:37 +0200

    Refactor test

--- test.js
+++ test.js
@@ -1,9 +1,18 @@
 'use strict';
 
-var spache, assert;
+/**
+ * Dependencies.
+ */
+
+var spache,
+    assert;
 
 spache = require('./');
 assert = require('assert');
+
+/**
+ * Tests.
+ */
 
 describe('spache', function () {
     it('should be an `Object`', function () {
@@ -12,17 +21,19 @@ describe('spache', function () {
 });
 
 describe('spache.is(word)', function () {
-    it('should return true if a word is listed in Spache', function () {
+    it('should return true if a word is listed', function () {
         assert(spache.is('train') === true);
     });
 
-    it('should return false if a word is not listed in Spache', function () {
+    it('should return false if a word is not listed', function () {
         assert(spache.is('unicorn') === false);
     });
 });
 
 describe('spache.all()', function () {
-    var all = spache.all();
+    var all;
+
+    all = spache.all();
 
     it('should return an array', function () {
         assert('length' in all);
@@ -30,21 +41,15 @@ describe('spache.all()', function () {
     });
 
     it('every entry should be lowercase', function () {
-        var iterator = -1,
-            length = all.length;
-
-        while (++iterator < length) {
-            assert(all[iterator].toLowerCase() === all[iterator]);
-        }
+        all.forEach(function (word) {
+            assert(word.toLowerCase() === word);
+        });
     });
 
     it('every entry should only occur once', function () {
-        var iterator = -1,
-            length = all.length;
-
-        while (++iterator < length) {
-            assert(all.indexOf(all[iterator], iterator + 1) === -1);
-        }
+        all.forEach(function (word, index) {
+            assert(all.indexOf(word, index + 1) === -1);
+        });
     });
 
     it('should be immutable', function () {
@@ -60,9 +65,11 @@ describe('spache.add(word) and spache.remove(word)',
             assert(spache.is('unicorn') === false);
 
             spache.add('unicorn');
+
             assert(spache.is('unicorn') === true);
 
             spache.remove('unicorn');
+
             assert(spache.is('unicorn') === false);
         });
 
@@ -71,10 +78,12 @@ describe('spache.add(word) and spache.remove(word)',
             assert(spache.is('rainbow') === false);
 
             spache.add('unicorn', 'rainbow');
+
             assert(spache.is('unicorn') === true);
             assert(spache.is('rainbow') === true);
 
             spache.remove('unicorn', 'rainbow');
+
             assert(spache.is('unicorn') === false);
             assert(spache.is('rainbow') === false);
         });
@@ -82,7 +91,9 @@ describe('spache.add(word) and spache.remove(word)',
         it('should fail silently when removing a non-existing word',
             function () {
                 assert(spache.is('unicorn') === false);
+
                 spache.remove('unicorn');
+
                 assert(spache.is('unicorn') === false);
             }
         );
