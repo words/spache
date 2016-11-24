93dc19e7ef407cc67701488a17b88d3f7afd10cb
Titus Wormer <tituswormer@gmail.com>
2016-11-24 18:52:43 +0100

    Remove superfluous comments

--- test.js
+++ test.js
@@ -1,18 +1,8 @@
-/**
- * @author Titus Wormer
- * @copyright 2014 Titus Wormer
- * @license MIT
- * @module spache
- * @fileoverview Test suite for `spache`.
- */
-
 'use strict';
 
-/* Dependencies. */
 var test = require('tape');
 var spache = require('./');
 
-/* Tests. */
 test('characterEntities', function (t) {
   t.equal(typeof spache, 'object', 'should be an array #1');
   t.equal(Array.isArray(spache), true, 'should be an array #2');
