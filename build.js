81997bfee0871c6d29285ccf757b39308afd3898
Titus Wormer <tituswormer@gmail.com>
2017-07-18 13:35:36 -0400

    Refactor build-script

--- build.js
+++ build.js
@@ -1,31 +1,38 @@
 'use strict';
 
 var fs = require('fs');
+var http = require('http');
 var bail = require('bail');
-var jsdom = require('jsdom');
+var concat = require('concat-stream');
+var unified = require('unified');
+var parse = require('rehype-parse');
+var selectAll = require('hast-util-select').selectAll;
+var toString = require('hast-util-to-string');
 
-jsdom.env('http://www.readabilityformulas.com/articles/spache-formula-word-list.php', read);
+http.get('http://www.readabilityformulas.com/articles/spache-formula-word-list.php', function (res) {
+  res.pipe(concat(onconcat)).on('error', bail);
 
-function read(err, window) {
-  bail(err);
+  function onconcat(buf) {
+    var tree = unified().use(parse).parse(buf);
+    var values = selectAll('td p', tree)
+      .map(toString)
+      .join('|')
+      .replace(/\\/g, '$&\'')
+      .trim()
+      .split(/\s*\|\s*/g)
+      .filter(Boolean)
+      .map(lower)
+      .filter(unique)
+      .sort();
 
-  var values = [].slice.call(window.document.querySelectorAll('td p'))
-    .map(function (node) {
-      return node.textContent;
-    })
-    .join('|')
-    .replace(/\\/g, '$&\'')
-    .trim()
-    .split(/\s*\|\s*/g)
-    .filter(Boolean)
-    .map(function (value) {
-      return value.toLowerCase();
-    })
-    .sort();
+    fs.writeFile('index.json', JSON.stringify(values, 0, 2) + '\n', bail);
+  }
+});
 
-  values = values.filter(function (value, index) {
-    return values.indexOf(value, index + 1) === -1;
-  });
+function lower(value) {
+  return value.toLowerCase();
+}
 
-  fs.writeFileSync('index.json', JSON.stringify(values, 0, 2) + '\n');
+function unique(value, index, all) {
+  return all.indexOf(value, index + 1) === -1;
 }
