aeaf11ee8ece71a681726259de20ce1262770f4d
Titus Wormer <tituswormer@gmail.com>
2022-11-07 09:12:53 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,12 +1,11 @@
-import test from 'tape'
+import assert from 'node:assert'
+import test from 'node:test'
 import {spache} from './index.js'
 
-test('spache', function (t) {
-  t.equal(typeof spache, 'object', 'should be an array #1')
-  t.equal(Array.isArray(spache), true, 'should be an array #2')
-  t.notEqual(spache.indexOf('train'), -1, 'should contain words')
-  t.notEqual(spache.indexOf("can't"), -1, "should contain can't")
-  t.equal(spache.indexOf("aren't"), -1, "should not contain aren't")
-
-  t.end()
+test('spache', function () {
+  assert.equal(typeof spache, 'object', 'should be an array #1')
+  assert.equal(Array.isArray(spache), true, 'should be an array #2')
+  assert.notEqual(spache.indexOf('train'), -1, 'should contain words')
+  assert.notEqual(spache.indexOf("can't"), -1, "should contain can't")
+  assert.equal(spache.indexOf("aren't"), -1, "should not contain aren't")
 })
